3573cb0f48469e63f4c176a8730105c34a13346a
Jared <allthosemoments@gmail.com>
2014-01-24 20:11:19 -0600

    some // cleanup

--- Tower Defense/src/Frame.java
+++ Tower Defense/src/Frame.java
@@ -1,29 +1,28 @@
 import java.awt.*;
 import javax.swing.*;
 
-
 public class Frame extends JFrame
 {
 	
 	public static String title = "Save the King";//title of game/file
 	public static Dimension size = new Dimension(700,550);//dimensions of frame
-	public Frame()//constructor
+	public Frame()        //constructor
 	{
-		setTitle(title);//sets title to title
-		setSize(size);// sets size to size
-		setResizable(false);//does not allow it to be resized
-		setLocationRelativeTo(null);//
+		setTitle(title);   //sets title to title
+		setSize(size);     // sets size to size
+		setResizable(false);
+		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//closes by closing window
 		
-		init();//calls in it
+		init();            //calls init
 	}
 	
-	public void init()//whats in the frame
+	public void init()//what's in the frame
 	{
-		setLayout(new GridLayout(1,1,0,0));//sets the grid layout to of the 1 by 1
-		Screen screen = new Screen(this);//creates a new screen
-		add(screen);//adds a screen
-		setVisible(true);//lets you see the frame
+		setLayout(new GridLayout(1,1,0,0));  //sets the grid layout to of the 1 by 1
+		Screen screen = new Screen(this);   //creates a new screen
+		add(screen);                         //adds a screen
+		setVisible(true);                    //lets you see the frame
 	}
 	
 	public static void main(String [] args)
