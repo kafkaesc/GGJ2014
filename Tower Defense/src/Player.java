af77e2890ca21f1c2b96e9c1968ce8e372d0673b
Jared <allthosemoments@gmail.com>
2014-01-25 18:21:22 -0600

    we now have player class, HP, resources, and manages red/blue

---
+++ Tower Defense/src/Player.java
@@ -0,0 +1,41 @@
+
+public class Player {
+
+	// instance vars
+	private int life;
+	private int resources;
+	public boolean isRed;
+	
+	Player()
+	{
+		life  = 50;
+		resources = 20;
+		isRed = false;
+	}
+	
+	public void burn(int n)
+	{
+		life -= n;
+		if(life < 0) life = 0;
+	}
+	
+	public int checkLife()
+	{
+		return life;
+	}
+	
+	public int checkResources()
+	{
+		return resources;
+	}
+	
+	public void profit(int n)
+	{
+		resources += n;
+	}
+	
+	public void switchGeneral() 
+	{ 
+		isRed = !isRed; 
+	}
+}
